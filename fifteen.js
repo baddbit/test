12d8f2aa20f5efe7ac19e4118900fbec61ffeb7c
gpc46003 <35608612+gpc46003@users.noreply.github.com>
2019-02-28 20:02:15 -0500

    Update fifteen.js

--- fifteen.js
+++ fifteen.js
@@ -1,1 +1,25 @@
+"use strict";
+
+function startPuzzle() {
+	document.getElementById("col_00").style.backgroundPosition = "0px 0px"; 
+	document.getElementById("col_01").style.backgroundPosition="-100px 0px";
+	document.getElementById("col_02").style.backgroundPosition="-200px 0px";
+	document.getElementById("col_03").style.backgroundPosition="-300px 0px";
+	document.getElementById("col_10").style.backgroundPosition="0px -100px";
+	document.getElementById("col_11").style.backgroundPosition="-100px -100px";
+	document.getElementById("col_12").style.backgroundPosition="-200px -100px";
+	document.getElementById("col_13").style.backgroundPosition="-300px -100px";
+	document.getElementById("col_20").style.backgroundPosition="0px -200px";
+	document.getElementById("col_21").style.backgroundPosition="-100px -200px";
+	document.getElementById("col_22").style.backgroundPosition="-200px -200px";
+	document.getElementById("col_23").style.backgroundPosition="-300px -200px";
+	document.getElementById("col_30").style.backgroundPosition="0px -300px";
+	document.getElementById("col_31").style.backgroundPosition="-100px -300px";
+	document.getElementById("col_32").style.backgroundPosition="-200px -300px";
+}
+
+
+function shufflePuzzle() {
+	
+}
 
